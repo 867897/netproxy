d4337a376a21b658ebe4e07126c4f50482b41e8d
867897 <99184318+867897@users.noreply.github.com>
2025-05-26 17:48:22 +0100

    Update sk5v2.sh

--- sk5v2/sk5v2.sh
+++ sk5v2/sk5v2.sh
@@ -1,4 +1,14 @@
-bash <(curl -Ls https://raw.githubusercontent.com/hasegawa-taku/v2ray/refs/heads/master/install.sh)
+sudo apt update
+#更新
+apt list --upgradable
+#更新运行一下
+#拉取
+wget https://raw.githubusercontent.com/867897/netproxy/refs/heads/main/sk5v2/install.sh
+#权限
+sudo chmod 777 install.sh
+#删除原有的配置
 v2ray d
+#bbr
 v2ray bbr
+#类型 端口 账号  密码
 v2ray add socks 6688 wenrou22 wenrou22
