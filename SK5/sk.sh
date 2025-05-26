2593635d0a618db0845a2a4699a3e7e31e61c454
867897 <99184318+867897@users.noreply.github.com>
2025-05-26 05:02:05 +0100

    Update sk.sh

--- SK5/sk.sh
+++ SK5/sk.sh
@@ -1,2 +1,3 @@
-wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh
+wget --no-check-certificate https://github.com/867897/netproxy/blob/main/SK5/install.sh
 bash install.sh  --port=1009 --user=lyh867897 --passwd=lyh867897
+cd 
