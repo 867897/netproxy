7d82e483143f7cb5e2042be677fa2a92fedbbd6a
867897 <99184318+867897@users.noreply.github.com>
2025-05-26 05:03:05 +0100

    Update sk.sh

--- SK5/sk.sh
+++ SK5/sk.sh
@@ -1,3 +1,3 @@
-wget --no-check-certificate https://github.com/867897/netproxy/blob/main/SK5/install.sh
+wget --no-check-certificate https://raw.githubusercontent.com/867897/netproxy/refs/heads/main/SK5/install.sh
 bash install.sh  --port=1009 --user=lyh867897 --passwd=lyh867897
 cd 
